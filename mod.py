4417fe7c25c63be9f03599166179ad2004894a04
unknown <iago.andradedf@gmail.com>
2021-02-22 22:24:38 -0300

    commit

--- mod.py
+++ mod.py
@@ -1,11 +1,11 @@
-def ExpBin(base, expoente, mod):
+def ExpBin(base, expp, mod):
     resp = 1
 
-    resp = (base ** expoente) % mod
+    resp = (base ** expp) % mod
 
     return resp
 
-base = int(input("Digite a base:"))
-expoente = int(input("Digite o expoente:"))
-mod = int(input("Digite o modulo:"))
-print(ExpBin(base,expoente,mod))
+base = int(input("Digite a base: "))
+expp = int(input("Digite o expoente: "))
+mod = int(input("Digite o modulo: "))
+print(ExpBin(base,expp,mod))
