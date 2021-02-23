4dacfe6305d0ea8e044421f19c8d3e38796a8758
Mateus <Mateus Moreira>
2021-02-22 23:13:48 -0300

    commit teste co-authored-by: philipe serafim <philipeserafim@gmail.com>

--- mod.py
+++ mod.py
@@ -6,6 +6,6 @@ def ExpBin(B, exp, mod):
     return resp
 
 base = int(input("Digite a base:"))
-expoente = int(input("Digite o expoente:aaaa"))
+expoente = int(input("Digite o expoente:aastesaaaa"))
 mod = int(input("Digite o MODULO123456:"))
 print(ExpBin(base,expoente,mod))
