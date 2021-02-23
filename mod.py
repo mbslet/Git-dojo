30b3b5d4ff3998fb353ef26021cec5782c79fd88
Mateus <Mateus Moreira>
2021-02-22 23:07:10 -0300

    testeee

--- mod.py
+++ mod.py
@@ -6,6 +6,6 @@ def ExpBin(base, expoente, mod):
     return resp
 
 base = int(input("Digite a base:"))
-expoente = int(input("Digite o expoente:"))
+expoente = int(input("Digite o expoenteeeeee:"))
 mod = int(input("Digite o MODULO123456:"))
 print(ExpBin(base,expoente,mod))
