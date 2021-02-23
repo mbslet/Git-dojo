e7bc446d8197ade82a9799ff9e2599bbde279246
Wesley Santos <wesley.sluk@hotmail.com>
2021-02-22 22:49:55 -0300

    alteracao

--- mod.py
+++ mod.py
@@ -1,11 +1,11 @@
-def ExpBin(base, expoente, mod):
+def ExpBin(base, expoente, modo):
     resp = 1
 
-    resp = (base ** expoente) % mod
+    resp = (base ** expoente) % modo
 
     return resp
 
 base = int(input("Digite a bas:"))
 expoente = int(input("Digite o exp:"))
-mod = int(input("Digite o mod:"))
-print(ExpBin(base,expoente,mod))
+modo = int(input("Digite o mod:"))
+print(ExpBin(base,expoente,modo))
